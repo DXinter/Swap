d3bcf67ae414d1b923aaf74047a0eb9bbc9f691d
DXinter <58430974+DXinter@users.noreply.github.com>
2019-12-09 18:11:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Swap
+{
+    /*
+     * функция Swap(string input), которая меняет местами первую и вторую половины строки (Swap("ABCD") -> "CDAB")
+     */
+    class Program
+    {
+        static void Main()
+        {
+            StringHelper helper = new StringHelper();
+            string SomeString = Set();
+            string final = helper.Swap(SomeString);
+            Console.WriteLine(final);
+        }
+
+        private static string Set()
+        {
+            Console.WriteLine("Введите строку: ");
+            string SomeString = Console.ReadLine();
+            return SomeString;
+        }
+    }
+}
---
+++ StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swap
+{
+    class StringHelper
+    {
+        public string Swap(string input)
+        {
+            string result = "";          
+            for (int i = input.Length / 2; i < input.Length; i++)
+            {
+                result += Convert.ToString(input[i]);
+            }
+            for (int i = 0; i < input.Length/2; i++) 
+            {
+                result += Convert.ToString(input[i]);                              
+            }       
+                return result;
+        }
+    }
+}
